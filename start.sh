f762dbf5858d57fc0095bc8337bf11d67dd52df0
Martijn van Maurik <martijn@vmaurik.nl>
2015-01-29 17:19:11 +0100

    Default exports exposure

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ mounts="/exports"
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
-  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" > /etc/exports
 done
 
 exec runsvdir /etc/sv
